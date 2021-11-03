b9ecf64b2fec594d76ee6b92001b91416f1ee874
BRB3D <samuraix221@hotmail.com>
2021-11-02 21:41:01 -0400

    eslint file

--- survey.js
+++ survey.js
@@ -12,9 +12,9 @@ rl.question(`What's your name? Nicknames are also acceptable 😁`, (answer) => 
         rl.question(`What's your favourite thing to eat for that meal? 🍗`, (answer5) => {
           rl.question(`Which sport is your absolute favourite? 🥊`, (answer6) => {
             rl.question(`What is your superpower? In a few words, tell us what you are amazing at!🦸`, (answer7) => {
-              console.log(`${answer} loves ${answer2}, while listening to ${answer3} devouring ${answer5} at ${answer4}, prefers ${answer6} over any other sport, and is amazing at ${answer7}`); 
+              console.log(`${answer} loves ${answer2}, while listening to ${answer3} devouring ${answer5} at ${answer4}, prefers ${answer6} over any other sport, and is amazing at ${answer7}`);
               
-              rl.close();  
+              rl.close();
             });
      
           });
@@ -23,8 +23,7 @@ rl.question(`What's your name? Nicknames are also acceptable 😁`, (answer) => 
      
       });
     });
-     
-   });
+  });
 });
 
 
